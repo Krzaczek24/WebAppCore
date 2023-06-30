1bfdd48a2aca7b524328d5bb17628477c5e6dd58
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-06-30 19:19:15 +0200

    Added nongeneric `ProducesResponseAttribute`

--- Core.WebApi/Attributes/ProducesResponseAttribute.cs
+++ Core.WebApi/Attributes/ProducesResponseAttribute.cs
@@ -3,6 +3,11 @@ using System.Net;
 
 namespace Core.WebApi.Attributes
 {
+    public class ProducesResponseAttribute : ProducesResponseTypeAttribute
+    {
+        public ProducesResponseAttribute(HttpStatusCode httpStatus) : base((int)httpStatus) { }
+    }
+
     public class ProducesResponseAttribute<TResponse> : ProducesResponseTypeAttribute
     {
         public ProducesResponseAttribute(HttpStatusCode httpStatus) : base(typeof(TResponse), (int)httpStatus) { }
